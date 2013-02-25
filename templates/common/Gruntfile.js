592be8daa230c74907e62e4e00271b19f4458ae5
Vitaly Domnikov <dotcypress@gmail.com>
2013-02-25 14:13:55 +0200

    Add clean task

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-mocha');
 
   var yeomanConfig = {
@@ -16,89 +17,92 @@ module.exports = function (grunt) {
   var PORT = 8765;
 
   grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  yeoman: yeomanConfig,
-  connect: {
-    server: {
-      options: {
-        port: PORT,
-        base: '.'
+    pkg: grunt.file.readJSON('package.json'),
+    yeoman: yeomanConfig,
+    connect: {
+      server: {
+        options: {
+          port: PORT,
+          base: '.'
+        }
       }
-    }
-  },
-  requirejs: {
-    compile: {
-      options: {
-        baseUrl: '.',
-        optimize: 'none',
-        paths: {
-          aura: 'app/components/aura/lib',
-          jquery: 'empty:',
-          underscore: 'empty:',
-          eventemitter: 'app/components/eventemitter2/lib/eventemitter2'
-        },
-        shim: {
-          underscore: {
-            exports: '_'
-          }
-        },
-        include: [
-          'app/main'
-        ],
-        exclude: ['jquery', 'aura/aura'],
-        out: 'dist/main.js'
-      }
-    }
-  },
-  copy: {
-    dist: {
-      files: [{
-        expand: true,
-        dot: true,
-        cwd: '<%= yeoman.app %>',
-        dest: '<%= yeoman.dist %>',
-        src: [
-          '*.{ico,txt,js,html}',
-          '.htaccess',
-          'components/**/*',
-          'images/**/*',
-          'styles/**/*',
-          'widgets/**/*',
-          'extensions/**/*'
-        ]
-      }]
-    }
-  },
-  jshint: {
-    all: {
-    options: {
-      jshintrc: '.jshintrc'
     },
-    files: {
-      src: [
-      'app/widgets/**.js',
-      'app/extensions/**.js',
-      'app/main.js',
-      ]
-    }
-    }
-  },
-  mocha: {
-    all: {
-      options: {
-        urls: ['http://localhost:<%= connect.server.options.port %>/spec/index.html']
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: '.',
+          optimize: 'none',
+          paths: {
+            aura: 'app/components/aura/lib',
+            jquery: 'empty:',
+            underscore: 'empty:',
+            eventemitter: 'app/components/eventemitter2/lib/eventemitter2'
+          },
+          shim: {
+            underscore: {
+              exports: '_'
+            }
+          },
+          include: [
+            'app/main'
+          ],
+          exclude: ['jquery', 'aura/aura'],
+          out: '<%= yeoman.dist %>/main.js'
+        }
       }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            '*.{ico,txt,js,html}',
+            '.htaccess',
+            'components/**/*',
+            'images/**/*',
+            'styles/**/*',
+            'widgets/**/*',
+            'extensions/**/*'
+          ]
+        }]
+      }
+    },
+    jshint: {
+      all: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        files: {
+          src: [
+          'app/widgets/**.js',
+          'app/extensions/**.js',
+          'app/main.js',
+          ]
+        }
+      }
+    },
+    mocha: {
+      all: {
+        options: {
+          urls: ['http://localhost:<%= connect.server.options.port %>/spec/index.html']
+        }
+      }
+    },
+    watch: {
+      files: [
+        '**/*.js'
+      ],
+      tasks: ['spec']
+    },
+    clean: {
+      dist: ['<%= yeoman.dist %>/**']
     }
-  },
-  watch: {
-    files: [
-      '**/*.js'
-    ],
-    tasks: ['spec']
-  }
   });
 
   grunt.registerTask('spec', ['jshint', 'connect', 'mocha']);
-  grunt.registerTask('build', ['spec', 'copy', 'requirejs']);
+  grunt.registerTask('build', ['clean', 'spec', 'copy', 'requirejs']);
   grunt.registerTask('default', ['spec', 'watch']);
 };
