79f4bf3d9f6db177ece8252fdf8dd9026cd38a6c
robertd <robert.djurasaj@gmail.com>
2013-08-01 23:15:56 -0600

    Rename WidgetGenerator to ComponentGenerator

--- component/index.js
+++ component/index.js
@@ -3,17 +3,17 @@ var path = require('path');
 var util = require('util');
 var yeoman = require('yeoman-generator');
 
-module.exports = WidgetGenerator;
+module.exports = ComponentGenerator;
 
-function WidgetGenerator() {
+function ComponentGenerator() {
   yeoman.generators.Base.apply(this, arguments);
   this.sourceRoot(path.join(path.dirname(__dirname), 'templates'));
   this.argument('name', { type: String, required: true });
 }
 
-util.inherits(WidgetGenerator, yeoman.generators.Base);
+util.inherits(ComponentGenerator, yeoman.generators.Base);
 
-WidgetGenerator.prototype.createAuraComponentFiles = function createAuraComponentFiles() {
+ComponentGenerator.prototype.createAuraComponentFiles = function createAuraComponentFiles() {
   this.template('aura_components/main.js', 'app/aura_components/' + this.name + '/main.js');
   // TODO: generate test
 };
